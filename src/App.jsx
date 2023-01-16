fa090016089a246ece99b2d9347cac958acb86f7
RimaSem <rima.semetaite@gmail.com>
2023-01-16 20:02:55 +0200

    Create Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header"
+import Card from "./components/Card"
 
 export default function App() {
 
   return (
     <div className="App">
       <Header/>
+      <section className="card-list">
+        <Card/>
+      </section>
     </div>
   )
 }
