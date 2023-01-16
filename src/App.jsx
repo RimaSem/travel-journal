f2a634a2b2908e571cb1a5f7a1331270c7bf1614
RimaSem <rima.semetaite@gmail.com>
2023-01-16 20:35:59 +0200

    Create cards array to pull data from data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./components/Header"
 import Card from "./components/Card"
+import data from "./data"
 
 export default function App() {
+  const cards = data.map(item => <Card {...item}/>)
 
   return (
     <div className="App">
       <Header/>
       <section className="card-list">
-        <Card/>
+        {cards}
       </section>
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,16 @@ import locationIcon from "../assets/location-icon.svg"
 
 export default function Card(props) {
     return <div className="card">
-        <img src="https://source.unsplash.com/WLxQvbMyfas"/>
+        <img src={props.imageUrl}/>
         <div className="card--right">
             <div className="location">
                 <img src={locationIcon} className="location--icon"/>
-                <div className="location--name">JAPAN</div>
-                <a href="https://goo.gl/maps/1DGM5WrWnATgkSNB8" className="location--link">View on Google Maps</a>
+                <div className="location--name">{props.location.toUpperCase()}</div>
+                <a href={props.googleMapsUrl} className="location--link">View on Google Maps</a>
             </div>
-            <h2 className="title">Mount Fuji</h2>
-            <p className="date">12 Jan, 2021 - 24 Jan, 2021</p>
-            <p className="text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            <h2 className="title">{props.title}</h2>
+            <p className="date">{props.startDate} - {props.endDate}</p>
+            <p className="text">{props.description}</p>
         </div>
     </div>
 }
